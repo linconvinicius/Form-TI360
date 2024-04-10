6afe0288e017cdf5484ed1604bfb5a9e9d6f0b78
linconvinicius <lvini_@live.com>
2024-04-10 08:49:15 -0300

    new scripts

--- script.js
+++ script.js
@@ -1,53 +1,70 @@
-function validateForm() {
-    var form = document.getElementById('myForm');
-    var firstName = form['firstName'].value.trim();
-    var lastName = form['lastName'].value.trim();
-    var seniority = form['seniority'].value.trim();
-    var email = form['email'].value.trim();
-    var question1 = form['question1'].value.trim();
-    var comments = form['comments'].value.trim();
-    var file = form['file'].files[0]; // Adicionando a variável para o arquivo
+let isPhoneRequired = false
 
-    // Verifica se pelo menos um campo obrigatório está preenchido
-    if (firstName === '' || lastName === '' || seniority === '' || email === '' || question1 === '' || !file) {
-        // Exibe um alerta indicando que todos os campos obrigatórios devem ser preenchidos
-        alert('Por favor, preencha todos os campos obrigatórios e selecione um arquivo.');
-        return false;
+const phoneLabelSpan = document.querySelector('.phone-label-span')
+const phoneField = document.getElementById('phone')
+
+document.querySelector('#phone-checkbox')
+  .addEventListener('change', function() {
+    if (this.checked) {
+      phoneLabelSpan.style.display = 'inline'
+      phoneField.required = !isPhoneRequired
+      isPhoneRequired = !isPhoneRequired
+    } else {
+      phoneLabelSpan.style.display = 'none'
+      phoneField.required = !isPhoneRequired
+      isPhoneRequired = !isPhoneRequired
     }
+  })
 
-    // Verifica se o arquivo é do tipo desejado
-    var allowedExtensions = /(\.pdf|\.docx|\.txt)$/i; // Extensões permitidas
-    if (!allowedExtensions.exec(file.name)) {
-        // Exibe um alerta indicando que o tipo de arquivo não é suportado
-        alert('Por favor, selecione um arquivo PDF, DOCX ou TXT.');
-        return false;
+document.querySelector('button[type="submit"]')
+  .addEventListener('click', function(event) {
+    event.preventDefault()
+    const firstNameField = document.getElementById('firstName')
+    const lastNameField = document.getElementById('lastName')
+    const emailField = document.getElementById('email')
+    const textareaField = document.getElementById('open-text-area')
+    const productField = document.getElementById('product')
+    const helpRadio = document.querySelector('input[value="ajuda"]')
+    const emailCheckbox = document.getElementById('email-checkbox')
+    const phoneCheckbox = document.getElementById('phone-checkbox')
+    const fileField = document.querySelector('input[type="file"]')
+    const successMessage = document.querySelector('.success')
+    if (!firstNameField.value || !lastNameField.value || !emailField.value || !textareaField.value) {
+      return showAndHideErrorMessage()
     }
+    if (isPhoneRequired && !phoneField.value) {
+      return showAndHideErrorMessage()
+    }
+    if (!emailField.value.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)) {
+      return showAndHideErrorMessage()
+    }
+    firstNameField.value = ''
+    lastNameField.value = ''
+    emailField.value = ''
+    textareaField.value = ''
+    phoneField.value = ''
+    productField.selectedIndex = 0
+    helpRadio.checked = true
+    emailCheckbox.checked = false
+    phoneCheckbox.checked = false
+    fileField.value = ''
+    phoneLabelSpan.style.display = 'none'
+    successMessage.style.display = 'block'
+    isPhoneRequired = false
+    scroll(0,0)
+    hideMessageAfterTimeout(successMessage)
+  }, false)
 
-    var resultsDiv = document.getElementById('formResults');
-
-    resultsDiv.innerHTML = `
-        <p><strong>Nome:</strong> ${firstName} ${lastName}</p>
-        <p><strong>Senioridade:</strong> ${seniority}</p>
-        <p><strong>Email:</strong> ${email}</p>
-        <p><strong>Qual sua escolha de área?</strong> ${question1}</p>        
-        <p><strong>Comentários:</strong> ${comments}</p>
-        <p><strong>Arquivo:</strong> ${file.name}</p>
-    `;
-
-    resultsDiv.style.display = 'block';
-
-    // Define um temporizador para ocultar a resposta após 5 segundos
-    setTimeout(function() {
-        resultsDiv.style.display = 'none';
-    }, 10000);
-
-    // Mostra o alerta de cadastro realizado com sucesso
-    alert('Cadastro realizado com sucesso!');
-
-    // Limpa o formulário após o envio
-    form.reset();
-
-    // Retorna false para evitar o envio do formulário
-    return false;
+function showAndHideErrorMessage() {
+  const errorMessage = document.querySelector('.error')
+  errorMessage.style.display = 'block'
+  scroll(0,0)
+  hideMessageAfterTimeout(errorMessage)
+  return
 }
 
+function hideMessageAfterTimeout(element) {
+  setTimeout(function() {
+    element.style.display = 'none'
+  }, 3000)
+}
